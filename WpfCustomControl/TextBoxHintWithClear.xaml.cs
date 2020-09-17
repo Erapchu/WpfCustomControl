5757a82d55b7698ff88b678a0459047ff172bdb0
Andrey Nasonov <andrey.nasonov@actonica.ru>
2020-09-17 14:24:37 +0700

    Added handler to clear button

--- WpfCustomControl/TextBoxHintWithClear.xaml.cs
+++ WpfCustomControl/TextBoxHintWithClear.xaml.cs
@@ -34,13 +34,46 @@ namespace WpfCustomControl
         public static readonly DependencyProperty HintProperty =
             DependencyProperty.Register("Hint", typeof(string), typeof(TextBoxHintWithClear), new PropertyMetadata());
 
-        public bool HasClearButton
+        public static readonly DependencyProperty HasClearButtonProperty =
+            DependencyProperty.Register("HasClearButton", typeof(bool), typeof(TextBoxHintWithClear), new PropertyMetadata(default(bool), HasClearButtonProperty_Changed));
+
+        public static bool GetHasClearButton(DependencyObject d)
         {
-            get { return (bool)GetValue(HasClearButtonProperty); }
-            set { SetValue(HasClearButtonProperty, value); }
+            return (bool)d.GetValue(HasClearButtonProperty);
         }
 
-        public static readonly DependencyProperty HasClearButtonProperty =
-            DependencyProperty.Register("HasClearButton", typeof(bool), typeof(TextBoxHintWithClear), new PropertyMetadata(default(bool)));
+        public static void SetHasClearButton(DependencyObject d, bool value)
+        {
+            d.SetValue(HasClearButtonProperty, value);
+        }
+
+        private static void HasClearButtonProperty_Changed(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (!(d is Control box))
+                return;
+
+            if (box.IsLoaded)
+                SetHasClearButtonHandler(box);
+            else
+                box.Loaded += (sender, args) =>
+                    SetHasClearButtonHandler(box);
+        }
+
+        public static void SetHasClearButtonHandler(Control box)
+        {
+            var bValue = GetHasClearButton(box);
+            var clearButton = box.Template.FindName("PART_ClearButton", box) as Button;
+            if (clearButton != null)
+            {
+                void handler(object sender, RoutedEventArgs args)
+                {
+                    (box as TextBox)?.SetCurrentValue(TextProperty, null);
+                }
+                if (bValue)
+                    clearButton.Click += handler;
+                else
+                    clearButton.Click -= handler;
+            }
+        }
     }
 }
