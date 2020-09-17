5f2920c62d9fb6a1079a71fc2721d1fd6e2edbba
Andrey Nasonov <andrey.nasonov@actonica.ru>
2020-09-17 13:54:42 +0700

    Added clear button to text box with hint

---
+++ WpfCustomControl/Converters/TextFieldClearButtonVisibilityConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace WpfCustomControl.Converters
+{
+    public class TextFieldClearButtonVisibilityConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool bAllTrue = true;
+            foreach (var v in values)
+            {
+                if (v is bool b && !b)
+                    bAllTrue = false;
+            }
+            return bAllTrue ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ WpfCustomControl/TextBoxHintWithClear.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfCustomControl
+{
+    /// <summary>
+    /// Interaction logic for TextBoxHintWithClear.xaml
+    /// </summary>
+    public partial class TextBoxHintWithClear : TextBox
+    {
+        public TextBoxHintWithClear()
+        {
+            InitializeComponent();
+        }
+
+        public string Hint
+        {
+            get { return (string)GetValue(HintProperty); }
+            set { SetValue(HintProperty, value); }
+        }
+
+        public static readonly DependencyProperty HintProperty =
+            DependencyProperty.Register("Hint", typeof(string), typeof(TextBoxHintWithClear), new PropertyMetadata());
+
+        public bool HasClearButton
+        {
+            get { return (bool)GetValue(HasClearButtonProperty); }
+            set { SetValue(HasClearButtonProperty, value); }
+        }
+
+        public static readonly DependencyProperty HasClearButtonProperty =
+            DependencyProperty.Register("HasClearButton", typeof(bool), typeof(TextBoxHintWithClear), new PropertyMetadata(default(bool)));
+    }
+}
