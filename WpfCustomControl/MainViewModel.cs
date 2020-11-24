c385f52f98585104902fca611e3ce31180efb9f7
Andrey Nasonov <andrey.nasonov@actonica.ru>
2020-11-24 10:14:19 +0700

    Added binding proxy, extend VM, added textblock highlighter

--- WpfCustomControl/MainViewModel.cs
+++ WpfCustomControl/MainViewModel.cs
@@ -3,10 +3,12 @@ using GalaSoft.MvvmLight.Command;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Data;
 
 namespace WpfCustomControl
 {
@@ -22,10 +24,49 @@ namespace WpfCustomControl
                 MessageBox.Show("It's working");
             }));
 
+        private string _filter;
+        private string _filterLower = string.Empty;
+        public string Filter
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(_filter)) _filter = string.Empty;
+                return _filter;
+            }
+            set
+            {
+                _filter = value;
+                _filterLower = _filter.ToLowerInvariant();
+                RaisePropertyChanged();
+                HeroesCollection.Refresh();
+            }
+        }
+
+        public ICollectionView HeroesCollection { get; }
+
         public MainViewModel()
         {
             for (int i = 0; i < 100; i++)
                 Collection.Add($"string {i}");
+
+            var heroes = new string[] 
+            { 
+                "Wonder woman",
+                "Iron man",
+                "Program",
+                "Ms Z",
+                "Black cat"
+            };
+
+            HeroesCollection = new ListCollectionView(heroes) 
+            {
+                Filter = FilterCollection
+            };
+        }
+
+        private bool FilterCollection(object obj)
+        {
+            return (obj as string).ToLowerInvariant().Contains(_filterLower);
         }
     }
 }
